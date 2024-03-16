d4a1eb5b68b7b83de9acd7717e34a9a1d70095cb
Dominik Stamm <dom8509@users.noreply.github.com>
2024-03-16 19:06:50 +0100

    removed wrong content

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,5 +1,5 @@
-import { QuartzConfig } from "./quartz/cfg"
-import * as Plugin from "./quartz/plugins"
+import { QuartzConfig } from './quartz/cfg';
+import * as Plugin from './quartz/plugins';
 
 /**
  * Quartz 4.0 Configuration
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dom8509.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
