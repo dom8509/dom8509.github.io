99a9d4dba2c4d4a90d33be02b15ffa11e62cef2d
Dominik Stamm <dom8509@users.noreply.github.com>
2024-03-16 19:19:29 +0100

    changed page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from './quartz/plugins';
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 dom8509.github.io",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
